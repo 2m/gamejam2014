30d53c0531a5eed0f8588af04cd30367e558fe8e
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2014-01-25 13:27:33 +0200

    Removes ; from srouce.

--- frontend/holocowst.js
+++ frontend/holocowst.js
@@ -1,14 +1,14 @@
 (function() {
-    var stage, car, angle = 0, speed = 0;
-    var l, r, u, d;
+    var stage, car, angle = 0, speed = 0
+    var l, r, u, d
 
     var myCarId = Math.floor((Math.random()*100)+1)
-    var counter = 0;
-    var socket = io.connect();
+    var counter = 0
+    var socket = io.connect()
     socket.on('news', function (data) {
-	console.log(data);
-	socket.emit('my other event', { id: "car_" + myCarId });
-    });
+	console.log(data)
+	socket.emit('my other event', { id: "car_" + myCarId })
+    })
 
     var otherCars = {}
     socket.on('other_car', function (data) {
@@ -21,11 +21,12 @@
 	}
 	else {
             console.log("creating new car " + data.id)
-            otherCar = new Sprite();
-            var cb = new Bitmap(new BitmapData("car.png"));
-            cb.x = -123; cb.y = -50;
-            otherCar.addChild(cb);
-            stage.addChild(otherCar);
+            otherCar = new Sprite()
+            var cb = new Bitmap(new BitmapData("car.png"))
+            cb.x = -123
+	    cb.y = -50
+            otherCar.addChild(cb)
+            stage.addChild(otherCar)
 
             otherCars[data.id] = otherCar
 	}
@@ -36,56 +37,58 @@
 
     function Start()
     {
-	stage = new Stage("c");
+	stage = new Stage("c")
 	
 	// background
-	var s = new Sprite();
-	s.graphics.beginBitmapFill(new BitmapData("asphalt.jpg"));
-	s.graphics.drawRect(0,0,stage.stageWidth, stage.stageHeight);
-	stage.addChild(s);
+	var s = new Sprite()
+	s.graphics.beginBitmapFill(new BitmapData("asphalt.jpg"))
+	s.graphics.drawRect(0,0,stage.stageWidth, stage.stageHeight)
+	stage.addChild(s)
 	
 	// car      
-	car = new Sprite(); 
-	car.x = stage.stageWidth/2;
-	car.y = stage.stageHeight/2;
-	var cb = new Bitmap(new BitmapData("car.png"));
-	cb.x = -123; cb.y = -50; car.addChild(cb);
-	stage.addChild(car);
+	car = new Sprite() 
+	car.x = stage.stageWidth/2
+	car.y = stage.stageHeight/2
+	var cb = new Bitmap(new BitmapData("car.png"))
+	cb.x = -123
+	cb.y = -50
+	car.addChild(cb)
+	stage.addChild(car)
 	
 	// events
-	stage.addEventListener(KeyboardEvent.KEY_DOWN, onKD);
-	stage.addEventListener(KeyboardEvent.KEY_UP  , onKU);
-	stage.addEventListener(Event.ENTER_FRAME     , onEF);
+	stage.addEventListener(KeyboardEvent.KEY_DOWN, onKD)
+	stage.addEventListener(KeyboardEvent.KEY_UP  , onKU)
+	stage.addEventListener(Event.ENTER_FRAME     , onEF)
     }
 
     function onKD (e)
     { 
-	if(e.keyCode == 37) l = true;
-	if(e.keyCode == 38) u = true;
-	if(e.keyCode == 39) r = true;
-	if(e.keyCode == 40) d = true;
+	if(e.keyCode == 37) l = true
+	if(e.keyCode == 38) u = true
+	if(e.keyCode == 39) r = true
+	if(e.keyCode == 40) d = true
     }
 
     function onKU (e)
     {
-	if(e.keyCode == 37) l = false;
-	if(e.keyCode == 38) u = false;
-	if(e.keyCode == 39) r = false;
-	if(e.keyCode == 40) d = false;
+	if(e.keyCode == 37) l = false
+	if(e.keyCode == 38) u = false
+	if(e.keyCode == 39) r = false
+	if(e.keyCode == 40) d = false
     }
 
     function onEF (e)
     {
-	speed *= 0.9;
-	if(u) speed += 1+speed*0.06;
-	if(d) speed -= 1;
+	speed *= 0.9
+	if(u) speed += 1+speed*0.06
+	if(d) speed -= 1
 	
-	if(r) angle += speed * 0.003;
-	if(l) angle -= speed * 0.003;
+	if(r) angle += speed * 0.003
+	if(l) angle -= speed * 0.003
 	
-	car.rotation = angle*180/Math.PI;
-	car.x += Math.cos(angle) * speed;
-	car.y += Math.sin(angle) * speed;
+	car.rotation = angle*180/Math.PI
+	car.x += Math.cos(angle) * speed
+	car.y += Math.sin(angle) * speed
 
 	counter = (counter + 1) % 50
 	if (counter == 0) {
@@ -99,4 +102,4 @@
     }
     
     Start()
-})();
+})()
